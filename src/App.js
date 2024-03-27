fdb1ab6d05b85f937e09cf9d9575d8ec79b95c44
ben_ono <bentoki.1213@gmail.com>
2024-03-26 23:33:31 -0700

    Create Card Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon, getPokemon } from './utils/pokemon';
+import Card from './components/Card/Card';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon'
@@ -37,7 +38,11 @@ function App() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <h1>ポケモンデータを取得しました。</h1>
+        <div className='pokemonCardContainer'>
+          {pokemonData.map((pokemon, i) => {
+            return <Card key={i} pokemon={pokemon}></Card>
+          })}
+        </div>
       )}
     </div>
   );
---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = ({pokemon}) => {
+  return (
+    <div>
+      Hogehoge
+    </div>
+  )
+}
+
+export default Card
+
