deeb327c43104311c7df9b49f0907d5ed8762c25
ben_ono <bentoki.1213@gmail.com>
2024-03-26 22:57:42 -0700

    Add getPokemon function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon'
   const [loading, setLoading] = useState(true)
+  const [pokemonData, setPokemonData] = useState([])
 
   useEffect(() => {
     const fetchPokemonData = async() => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
+      // 各ポケモンの詳細なデータを取得
+      console.log('load')
+      loadPokemon(res.results);
       console.log(res);
       setLoading(false)
     }
     fetchPokemonData()
-
   }, []) // 1回だけ呼び出したいので第二引数はからの配列※Why?
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        //console.log(pokemon.url)
+        let pokemonRecord = getPokemon(pokemon.url)
+        return pokemonRecord
+      })
+    )
+    setPokemonData(_pokemonData)
+  }
+  console.log(pokemonData)
 
   return (
     <div className="App">
