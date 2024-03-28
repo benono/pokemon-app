8403d86ea1727c370d50d485bc01762e251a2971
ben_ono <bentoki.1213@gmail.com>
2024-03-28 14:27:11 -0700

    Add paging button

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon'
   const [loading, setLoading] = useState(true)
   const [pokemonData, setPokemonData] = useState([])
+  const [nextURL, setNextURL] = useState('')
+  const [prevURL, setPrevURL] = useState('')
 
   useEffect(() => {
     const fetchPokemonData = async() => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
       // 各ポケモンの詳細なデータを取得
-      console.log('load')
+      
       loadPokemon(res.results);
-      console.log(res);
+      setNextURL(res.next)
+      setPrevURL(res.previous)
       setLoading(false)
     }
     fetchPokemonData()
@@ -32,7 +35,26 @@ function App() {
     )
     setPokemonData(_pokemonData)
   }
-  console.log(pokemonData)
+  // console.log(pokemonData)
+
+  const handleNextPave = async () => {
+    setLoading(true)
+    let data = await getAllPokemon(nextURL)
+    await loadPokemon(data.results)
+    setNextURL(data.next)
+    setPrevURL(data.previous)
+    setLoading(false)
+  }
+
+  const handlePrePave = async () => {
+    if (!prevURL) return
+    setLoading(true)
+    let data = await getAllPokemon(prevURL)
+    await loadPokemon(data.results)
+    setNextURL(data.next)
+    setPrevURL(data.previous)
+    setLoading(false)
+  }
 
   return (
     <>
@@ -41,11 +63,17 @@ function App() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
+        <>
         <div className='pokemonCardContainer'>
           {pokemonData.map((pokemon, i) => {
             return <Card key={i} pokemon={pokemon}></Card>
           })}
         </div>
+        <div className='btn'>
+          <button onClick={handlePrePave}>前へ</button>
+          <button onClick={handleNextPave}>次へ</button>
+        </div>
+        </>
       )}
     </div>
     </>
